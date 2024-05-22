45db3245f341f424fc3534a3e4ece6fa081e3be9
KristalinZH <kristalinzhelezchev@gmail.com>
2024-05-22 23:27:13 +0300

    Attempt to request data from backend and display it

--- src/front-end/src/routes/+layout.svelte
+++ src/front-end/src/routes/+layout.svelte
@@ -1,4 +1,11 @@
-<script>
-    /** @type {import('./$types').LayoutData} */
-    export let data;
-</script>
+<nav>
+    <ul>
+        <a href="./">Home</a>
+        <a href="./chats">Chats</a>
+    </ul>
+</nav>
+
+<div class="right">
+    <h2>This is my layout page</h2>
+</div>
+<slot></slot>
---
+++ src/front-end/src/routes/chats/+layout.svelte
@@ -0,0 +1,5 @@
+<script>
+
+</script>
+<h2>My second laoyuot</h2>
+<slot></slot>
