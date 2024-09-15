4e12a1c8fb0098d877b73906fcfd9060c21bbc74
KristalinZH <kristalinzhelezchev@gmail.com>
2024-09-15 17:21:13 +0300

    Add tailwind, eslint and remove files

--- src/front-end/src/routes/+layout.svelte
+++ src/front-end/src/routes/+layout.svelte
@@ -1,11 +1,5 @@
-<nav>
-    <ul>
-        <a href="./">Home</a>
-        <a href="./chats">Chats</a>
-    </ul>
-</nav>
-
-<div class="right">
-    <h2>This is my layout page</h2>
-</div>
-<slot></slot>
+<script>
+    import "../app.css";
+</script>
+  
+<slot />
--- src/front-end/src/routes/chats/+layout.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-
-</script>
-<h2>My second laoyuot</h2>
-<slot></slot>
